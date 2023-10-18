d8e529991b62cc98a3b7fcfaa99c824fec9565ff
jrs <hello@jrs.dev>
2023-10-18 12:01:01 -0700

    Left off at 3.2.5

--- Sounds and Effects/Assets/Scripts/MoveLeft.cs
+++ Sounds and Effects/Assets/Scripts/MoveLeft.cs
@@ -5,16 +5,20 @@ using UnityEngine;
 public class MoveLeft : MonoBehaviour
 {
     private float _speed = 30f;
+    private PlayerController _playerControllerScript;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.left * _speed * Time.deltaTime);
+        if (_playerControllerScript.GameOver == false)
+        {
+            transform.Translate(Vector3.left * _speed * Time.deltaTime);
+        }
     }
 }
--- Sounds and Effects/Assets/Scripts/PlayerController.cs
+++ Sounds and Effects/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     public float JumpForce = 10f;
     public float GravityModifier = 1f;
     public bool IsOnGround = true;
+    public bool GameOver = false;
 
     private Rigidbody _playerRb;
 
@@ -29,6 +30,14 @@ public class PlayerController : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        IsOnGround = true;
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            IsOnGround = true;
+        }
+        else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            GameOver = true;
+            Debug.Log("Game Over");
+        }
     }
 }
